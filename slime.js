0eb4498480d60811f1dd72880cfb3d4f5ce50835
Philip Jagielski <philipjagielski@gmail.com>
2012-01-09 21:24:08 -0600

    Formatting commit: I did the initial version of this in jsFiddle and it uses a 4 space indent while I prefer 2

--- slime.js
+++ slime.js
@@ -1,26 +1,26 @@
 var drawBackground = function(ctx) {
-    // blue sky
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(0, 0, 750, 275);
-    
-    // grey ground
-    ctx.fillStyle = 'rgb(128, 128, 128)';
-    ctx.fillRect(0, 275, 750, 100);
-    
-    // white fence in the middle
-    fenceWidth = 4;
-    fenceTop = 40;
-    fenceBottom = 4;
-    ctx.fillStyle = 'white';
-    ctx.fillRect(750/2 - fenceWidth/2, 275 - fenceTop, fenceWidth, fenceTop + fenceBottom);
+  // blue sky
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0, 0, 750, 275);
+  
+  // grey ground
+  ctx.fillStyle = 'rgb(128, 128, 128)';
+  ctx.fillRect(0, 275, 750, 100);
+  
+  // white fence in the middle
+  fenceWidth = 4;
+  fenceTop = 40;
+  fenceBottom = 4;
+  ctx.fillStyle = 'white';
+  ctx.fillRect(750/2 - fenceWidth/2, 275 - fenceTop, fenceWidth, fenceTop + fenceBottom);
 };
 
 var drawBall = function(ctx, xPos, height) {
-    var radius = 10;
-    ctx.beginPath();
-    ctx.fillStyle = 'yellow';
-    ctx.arc(xPos, 275 - height, radius, 0, 2*Math.PI, true);
-    ctx.fill();
+  var radius = 10;
+  ctx.beginPath();
+  ctx.fillStyle = 'yellow';
+  ctx.arc(xPos, 275 - height, radius, 0, 2*Math.PI, true);
+  ctx.fill();
 };
 
 $(document).ready(function() {
@@ -34,14 +34,14 @@ $(document).ready(function() {
   var KEYS = {};
 
   var mainLoop = function() {
-      drawBackground(ctx);
-      
-      player1.move(KEYS);
-      player1.draw(ctx);
-      player2.move(KEYS);
-      player2.draw(ctx);
-      
-      drawBall(ctx, 100, 50);
+    drawBackground(ctx);
+    
+    player1.move(KEYS);
+    player1.draw(ctx);
+    player2.move(KEYS);
+    player2.draw(ctx);
+    
+    drawBall(ctx, 100, 50);
   };
   mainLoop();
   mainLoopTimer = window.setInterval(mainLoop, 20);
